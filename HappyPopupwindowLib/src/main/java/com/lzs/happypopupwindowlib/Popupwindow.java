180c1d1825845e9da65c5b377dc6cfe79dc7f250
liuzishuo0603 <705377068@qq.com>
2021-04-14 14:31:56 +0800

    创建模板
    初次尝试

---
+++ HappyPopupwindowLib/src/main/java/com/lzs/happypopupwindowlib/Popupwindow.java
@@ -0,0 +1,13 @@
+package com.lzs.happypopupwindowlib;
+
+/**
+ * title： Administrator
+ * author: zsliu
+ * created by zsliu on 2021/4/14 14:20
+ * description: 弹窗
+ */
+public class Popupwindow {
+    public void log() {
+        System.out.println("我是日志哦");
+    }
+}
---
+++ app/src/main/java/com/lzs/happypopupwindow/Test.java
@@ -0,0 +1,15 @@
+package com.lzs.happypopupwindow;
+
+import com.lzs.happypopupwindowlib.Popupwindow;
+
+/**
+ * title： Administrator
+ * author: zsliu
+ * created by zsliu on 2021/4/14 14:28
+ * description: 测试
+ */
+public class Test {
+    public static void main(String[] args) {
+        new Popupwindow().log();
+    }
+}
